da70b4eaa5de6ed1fce9ba68697cb5bb8b876c39
Sorin Florea <sorin.florea@vdx.tv>
2020-05-18 22:29:50 +0300

    Add homepage components structure and styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+import "../../components/directory/directory.component";
+import Direcytory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Direcytory />
+  </div>
+);
+
+export default HomePage;
